cc486ddb1c03fc89d3e9ca6da9f47a9a37c706ab
chg1f <chongiofai@gmail.com>
2022-05-05 11:27:44 +0800

    refactor!: compress, shrink

--- error.go
+++ error.go
@@ -5,17 +5,65 @@ import (
 	"strings"
 )
 
-// FIXME: WeakError?CompressError?
+type CompressError struct {
+	Errors []error
+}
 
-func Compress(es ...error) error {
-	t := make([]string, 0, len(es))
-	for _, e := range es {
-		if e != nil {
-			t = append(t, e.Error())
+func (ce *CompressError) Is(t error) bool {
+	if t == nil {
+		if ce == nil {
+			return true
+		}
+	} else if ce.Errors == nil {
+		return false
+	}
+	for _, e := range ce.Errors {
+		if errors.Is(e, t) {
+			return true
 		}
 	}
-	if len(t) == 0 {
-		return nil
+	return false
+}
+func (ce *CompressError) As(t interface{}) bool {
+	if ce == nil {
+		return false
 	}
-	return errors.New(strings.Join(t, "; "))
+	for _, e := range ce.Errors {
+		if errors.As(e, t) {
+			return true
+		}
+	}
+	return false
+}
+func (ce *CompressError) Error() string {
+	temp := make([]string, 0, len(ce.Errors))
+	for _, e := range ce.Errors {
+		if e != nil {
+			temp = append(temp, e.Error())
+		}
+	}
+	return strings.Join(temp, "; ")
+}
+
+func Compress(es ...error) error {
+	ce := CompressError{
+		Errors: make([]error, 0, len(es)),
+	}
+	for _, e := range es {
+		if e != nil {
+			if t, ok := e.(*CompressError); ok {
+				ce.Errors = append(ce.Errors, t.Errors...)
+				continue
+			}
+			ce.Errors = append(ce.Errors, e)
+		}
+	}
+	if len(ce.Errors) > 0 {
+		return &ce
+	}
+	return nil
+}
+
+func Shrink(es ...error) error {
+	return errors.New(Compress(es...).Error())
 }
--- error_test.go
+++ error_test.go
@@ -2,15 +2,68 @@ package errorx
 
 import (
 	"errors"
+	"fmt"
+	"math/rand"
+	"os"
 	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
+func TestShrink(t *testing.T) {
+	es := []error{os.ErrNotExist, os.ErrExist}
+	temp := make([]string, 0, len(es))
+	for _, e := range es {
+		temp = append(temp, e.Error())
+	}
+	text := strings.Join(temp, "; ")
+	ce := Compress(es...)
+
+	assert.Equal(t, text, ce.Error())
+	assert.Equal(t, text, Shrink(ce).Error())
+
+	fmt.Println(ce.Error())
+}
+
+func TestCompress(t *testing.T) {
+	errPath0 := &os.PathError{Path: strconv.FormatUint(rand.Uint64(), 16), Err: os.ErrExist}
+	errPath1 := &os.PathError{Path: strconv.FormatUint(rand.Uint64(), 16), Err: os.ErrExist}
+	ce := Compress(os.ErrExist, errPath0, errPath1)
+
+	assert.True(t, errors.Is(ce, os.ErrExist))
+	assert.True(t, errors.Is(ce, errPath0))
+	assert.True(t, errors.Is(ce, errPath1))
+	assert.False(t, errors.Is(ce, os.ErrNotExist))
+
+	{
+		var e *os.PathError
+		assert.True(t, errors.As(ce, &e))
+		assert.Equal(t, errPath0.Path, e.Path)
+		assert.NotEqual(t, errPath1.Path, e.Path)
+	}
+	{
+		var e *CompressError
+		assert.True(t, errors.As(ce, &e))
+		var e1 *os.PathError
+		assert.True(t, errors.As(e.Errors[2], &e1))
+		assert.Equal(t, errPath1.Path, e1.Path)
+	}
+}
+
+func TestCompressNil(t *testing.T) {
+	assert.Nil(t, Compress(nil, nil))
+	assert.Len(t, Compress(nil, os.ErrClosed).(*CompressError).Errors, 1)
+}
+
+func ExampleShrink() {
+	fmt.Println(Shrink(os.ErrNotExist, os.ErrExist))
+	// output: file does not exist; file already exists
+}
+
 func ExampleCompress() {
-	var err error
-	for i := 0; i < 5; i += 1 {
-		err = Compress(err, errors.New(strconv.Itoa(i)))
-	}
-	if err != nil {
-		// error
-	}
+	ce := Compress(os.ErrNotExist, os.ErrExist)
+	fmt.Println(ce)
+	// output: file does not exist; file already exists
 }
