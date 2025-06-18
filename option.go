8927f707d9af9735f612ec5f29d867cb62cbe7dd
chg1f <chongiofai@gmail.com>
2025-06-18 20:18:04 +0800

    feat: code option

--- option.go
+++ option.go
@@ -5,36 +5,31 @@ type CodeOption[T comparable] struct {
 	NaN   T
 }
 
-func NaN[T comparable](code T) CodeOption[T] {
-	return CodeOption[T]{
-		NaN: code,
+func NaN[T comparable](code T) func(*CodeOption[T]) {
+	return func(opt *CodeOption[T]) {
+		opt.NaN = code
 	}
 }
 
-func Empty[T comparable](code T) CodeOption[T] {
-	return CodeOption[T]{
-		Empty: code,
+func Empty[T comparable](code T) func(*CodeOption[T]) {
+	return func(opt *CodeOption[T]) {
+		opt.Empty = code
 	}
 }
 
-func CodeOf[T comparable](err error, opts ...CodeOption[T]) T {
-	if len(opts) > 1 {
-		panic("only one option is allowed")
+func CodeOf[T comparable](err error, opts ...func(*CodeOption[T])) T {
+	var opt CodeOption[T]
+	for i := range opts {
+		opts[i](&opt)
 	}
-	var empty T
 	ex := Be[T](err)
 	if ex == nil {
-		if len(opts) == 1 {
-			return opts[0].NaN
-		}
-		return empty
+		return opt.NaN
 	}
+	var empty T
 	code := ex.Code()
 	if ex.code == empty {
-		if len(opts) == 1 {
-			return opts[0].Empty
-		}
-		return empty
+		return opt.Empty
 	}
 	return code
 }
